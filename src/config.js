d08567d1866249ab4d909340ddc375c9678cb80a
Sangita Setlur <sangitasetlur@Sangitas-MacBook-Pro.local>
2020-06-07 12:05:09 -0500

    Setting up the React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-sangita"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://b9134ok9id.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_9EfXXbGoq",
+    APP_CLIENT_ID: "19mkuov2nhqknmg1t1odi1v6i8",
+    IDENTITY_POOL_ID: "us-east-1:68be346d-b0c0-43fa-ac6a-1b37c0c0a897"
+  }
+};
