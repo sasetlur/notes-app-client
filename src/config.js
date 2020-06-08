4e60df2a391b6e2108122f657e2f7c1f534c0c6b
Sangita Setlur <sangitasetlur@Sangitas-MacBook-Pro.local>
2020-06-08 12:13:05 -0500

    Building our React app

--- src/config.js
+++ src/config.js
@@ -12,5 +12,6 @@ export default {
     USER_POOL_ID: "us-east-1_9EfXXbGoq",
     APP_CLIENT_ID: "19mkuov2nhqknmg1t1odi1v6i8",
     IDENTITY_POOL_ID: "us-east-1:68be346d-b0c0-43fa-ac6a-1b37c0c0a897"
-  }
+  },
+  STRIPE_KEY: "pk_test_51GqrYvF1HsTeT1hLKmZ5TIiWpeVwDKCbPuhIXzxQyfs6UJF9g98OypLsgYMBAsT5tyGUs2k3muig571eJ24rlEQv00lFkFZaQv"
 };
